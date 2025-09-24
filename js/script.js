bee8083a985aed98a6398c595ca60bd72e4189f8
1Sam1 <68424755+1Sam1@users.noreply.github.com>
2025-09-24 20:47:23 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
-const sidebar = document.getElementById("sidebar");
-const content = document.getElementById("content");
-const menuBtn = document.getElementById("menu-btn");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-menuBtn.onclick = function() {
-    if (sidebar.style.width === "250px") {
-        sidebar.style.width = "0";
-        content.style.marginLeft = "0";
-    } else {
-        sidebar.style.width = "250px";
-        content.style.marginLeft = "250px";
-    }
-}
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
